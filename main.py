9c6066245b7570652ad8ca587dd498427fbb49f7
SlowDeath67 <85418974+SlowDeath67@users.noreply.github.com>
2021-12-21 16:04:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+GNU nano 6.0                        main.py
 import subprocess
 import os
 subprocess.run("pip install pyfiglet".split())
 os.system("clear")
+import pyfiglet
 print(pyfiglet.figlet_format("Karim Badouri"))
 
-subprocess.run("python ./telegram/bot.py")
+subprocess.run("python ./telegram/bot.py".split())
