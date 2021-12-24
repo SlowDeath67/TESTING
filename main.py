0af2c15d5442da0ffba445164dfee37ffb51ff7f
SlowDeath67 <85418974+SlowDeath67@users.noreply.github.com>
2021-12-24 16:22:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import os
 subprocess.run("pip install pyfiglet".split())
 os.system("clear")
 import pyfiglet
-print(pyfiglet.figlet_format("Karim Badouri"))
 
-subprocess.run("python bot.py".split())
+def logo():
+  print("""
+_  __          _             ____            _                  _
+| |/ /__ _ _ __(_)_ __ ___   | __ )  __ _  __| | ___  _   _ _ __(_)
+| ' // _` | '__| | '_ ` _ \  |  _ \ / _` |/ _` |/ _ \| | | | '__| |
+| . \ (_| | |  | | | | | | | | |_) | (_| | (_| | (_) | |_| | |  | |
+|_|\_\__,_|_|  |_|_| |_| |_| |____/ \__,_|\__,_|\___/ \__,_|_|  |_|
+
+\tTelegram : @UltraCode000
+\tWhatsApp : +21652685817""")
+  
+def main():
+  logo()
+  subprocess.run("python bot.py".split())
+  
+main()
